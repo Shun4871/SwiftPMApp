35def5a7201d64a461d17f3419edf100e4316dae
ShunnosukeTsuge <shuntsugework@gmail.com>
2024-09-16 06:20:25 +0900

    Initial Commit

---
+++ SwiftPMApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftPMApp
+//
+//  Created by 柘植俊之介 on 2024/09/16.
+//
+
+import UIKit
+import PKHUD
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        HUD.flash(.success, delay: 2.0)
+    }
+
+
+}
+
